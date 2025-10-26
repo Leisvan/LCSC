252f4a2df4b373fd33a16363f7e24970fe8fb655
Leisvan Cordero <rathma@live.com>
2025-10-25 22:16:23 -0600

    Removed bundle step

---
+++ LCSC.App/Services/DispatcherService.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.UI.Xaml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LCSC.App.Services;
+
+public class DispatcherService
+{
+    private readonly DispatcherTimer _timer;
+
+    public DispatcherService()
+    {
+        _timer = new DispatcherTimer();
+    }
+}
