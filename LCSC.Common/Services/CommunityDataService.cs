abdca454f95f382cf092774457f16e679f71a1bf
Leisvan Cordero <rathma@live.com>
2025-12-08 15:46:09 -0600

    Add progress reporting and data refresh to update method
    Added optional progress report callback invocation with a status message before updating. Also ensured data is refreshed by calling RefreshAllAsync() prior to processing member profiles.

--- LCSC.Common/Services/CommunityDataService.cs
+++ LCSC.Common/Services/CommunityDataService.cs
@@ -223,6 +223,12 @@ public class CommunityDataService(LadderService ladderService, CacheService cach
         Func<RegionUpdateProgressReportData, Task>? progressReport = null,
         CancellationToken? token = null)
     {
+        if (progressReport != null)
+        {
+            await progressReport.Invoke(RegionUpdateProgressReportData.Message("Obteniendo información actualizada"));
+        }
+        await RefreshAllAsync();
+
         var profilesList = _members
             .Where(m => includeBannedPlayers || !m.Record.Banned)
             .Where(m => m.Profiles != null && m.Profiles.Count > 0)
